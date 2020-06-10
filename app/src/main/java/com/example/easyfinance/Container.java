f025dff8f0d2fd9d321bf8d0cdefbf834cdd951e
titusv <49645680+titusv@users.noreply.github.com>
2020-06-10 17:33:40 +0200

    - connected Activitys with xml files - made container as a singelton

--- app/src/main/java/com/example/easyfinance/Container.java
+++ app/src/main/java/com/example/easyfinance/Container.java
@@ -1,8 +1,12 @@
 package com.example.easyfinance;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 
 public class Container {
+
+    final static String TAG = "CONTAINER";
     Container instance;
     ArrayList<Receipt> arrayList;
 
@@ -15,4 +19,23 @@ public class Container {
         }
         return instance;
     }
+    public Receipt getReciept(int index){
+        if(arrayList.get(index)!=null){
+            return arrayList.get(index);
+        }
+        Log.d(TAG, "getReciept failed! No Reciept found! Index is out of Range");
+        return null;
+    }
+    public Receipt deleteReciept(int index){
+        if(arrayList.get(index)!=null){
+            return arrayList.remove(index);
+        }
+        Log.d(TAG,"deleteReciept failed! No Reciept found! Index is out of Range");
+        return null;
+    }
+    public Receipt search(String catchphrase){
+        // searchfunction
+        return null;
+    }
+
 }
