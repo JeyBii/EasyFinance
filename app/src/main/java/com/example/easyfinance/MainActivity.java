0e5f6f27c934615acf64b6765be2952dbe89aa68
titusv <49645680+titusv@users.noreply.github.com>
2020-06-10 18:52:55 +0200

    - test button added

--- app/src/main/java/com/example/easyfinance/MainActivity.java
+++ app/src/main/java/com/example/easyfinance/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.easyfinance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this,ScanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
