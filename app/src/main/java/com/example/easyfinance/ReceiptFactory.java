1bd62e8137a25def83a7704e6937694383122f3d
jburdi2s <28tZv>
2020-06-10 17:49:52 +0200

    test

--- app/src/main/java/com/example/easyfinance/ReceiptFactory.java
+++ app/src/main/java/com/example/easyfinance/ReceiptFactory.java
@@ -1,4 +1,22 @@
 package com.example.easyfinance;
 
 public class ReceiptFactory {
+
+    private ReceiptFactory instance = null;
+
+    private ReceiptFactory() {
+    }
+    public ReceiptFactory getInstance() {
+
+        if (instance == null)
+            instance = new ReceiptFactory();
+
+        return instance;
+    }
+
+    public void createReceipt(String imagePath, String shop) {
+
+        Receipt receipt = new Receipt(imagePath, shop);
+
+    }
 }
