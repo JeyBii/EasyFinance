06e55952ee6b134a1c561dad4d434e955df928f4
titusv <49645680+titusv@users.noreply.github.com>
2020-06-10 17:08:28 +0200

    - connected Activitys with xml files - made container as a singelton

--- app/src/main/java/com/example/easyfinance/Container.java
+++ app/src/main/java/com/example/easyfinance/Container.java
@@ -1,5 +1,18 @@
 package com.example.easyfinance;
 
+import java.util.ArrayList;
+
 public class Container {
-    int i = 1;
+    Container instance;
+    ArrayList<Receipt> arrayList;
+
+    private Container(){
+        arrayList = new ArrayList<Receipt>();
+    }
+    public Container getInstance(){
+        if(instance == null){
+            instance = new Container();
+        }
+        return instance;
+    }
 }
--- app/src/main/java/com/example/easyfinance/PaymentActivity.java
+++ app/src/main/java/com/example/easyfinance/PaymentActivity.java
@@ -1,4 +1,14 @@
 package com.example.easyfinance;
 
-public class PaymentActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
 }
